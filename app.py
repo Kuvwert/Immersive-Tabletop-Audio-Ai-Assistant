6ec80a4e5267341b58883f53d252ae00a1aa2131
Kuvwert <140937030+Kuvwert@users.noreply.github.com>
2023-10-28 17:39:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,107 +1,66 @@
-import pyaudio
+import openai
 import speech_recognition as sr
 import pygame
-import requests
 import json
-import time
-import keyboard
 
+# OpenAI API key
+openai.api_key = "YOUR_OPENAI_API_KEY"
 
-class AudioHandler:
-    def capture_audio(self):
-        recognizer = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening...")
-            try:
-                audio_data = recognizer.listen(source, timeout=10)
-                print("Audio captured.")
-                return audio_data
-            except sr.WaitTimeoutError:
-                print("Timeout: No speech detected.")
-                return None
+# Load the commands mapping
+with open("commands.json", "r") as file:
+    commands_mapping = json.load(file)
 
-    def transcribe_audio(self, audio_data):
-        recognizer = sr.Recognizer()
-        try:
-            text = recognizer.recognize_google(audio_data)
-            print(f"Transcribed text: {text}")
-            return text
-        except sr.UnknownValueError:
-            print("Speech Recognition could not understand audio.")
-            return None
-        except sr.RequestError:
-            print("API unavailable.")
-            return None
+def listen_and_play_audio():
+    """
+    Listen for audio input, recognize it, determine theme using OpenAI, and play corresponding audio clip.
+    """
+    # Create speech recognizer object
+    r = sr.Recognizer()
 
+    # Listen for input
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
 
-class NLPService:
-    OPENAI_API_ENDPOINT = "https://api.openai.com/v1/engines/davinci-codex/completions"
-    OPENAI_API_KEY = "YOUR_OPENAI_API_KEY"  # Replace with your OpenAI API key
+    # Try to recognize the audio
+    try:
+        spoken_text = r.recognize_google(audio, language="en-EN", show_all=False)
+        print("You mentioned:", spoken_text)
 
-    def process_text(self, text, commands_mapping):
+        # Create a prompt for OpenAI
         themes_list = ', '.join(commands_mapping.keys())
-        prompt_text = f"Match this sentence to a sound from this list: {text}, {themes_list}. Respond only with the word from the list. No additional words or context."
+        prompt_text = f"Match this sentence to a sound from this list: {spoken_text}, {themes_list}. Respond only with the word from the list. No additional words or context."
 
-        headers = {
-            "Authorization": f"Bearer {self.OPENAI_API_KEY}",
-            "Content-Type": "application/json",
-            "User-Agent": "OpenAI Python Client"
-        }
-        data = {
-            "prompt": prompt_text,
-            "max_tokens": 50
-        }
+        # Use OpenAI to match the theme
+        response = openai.Completion.create(
+            engine="davinci-codex",
+            prompt=prompt_text,
+            temperature=0.7,
+            max_tokens=50
+        )
 
-        try:
-            response = requests.post(self.OPENAI_API_ENDPOINT, headers=headers, json=data)
-            response.raise_for_status()
+        # Extract the matched theme
+        matched_theme = str(response['choices'][0]['text']).strip()
+        print(f"Matched theme: {matched_theme}")
 
-            matched_theme = response.json()["choices"][0]["text"].strip()
-            print(f"Received matched theme from GPT-3.5 Turbo: {matched_theme}")
-            return matched_theme
+        # Play the corresponding audio clip
+        audio_clip_path = commands_mapping.get(matched_theme)
+        if audio_clip_path:
+            pygame.mixer.init()
+            pygame.mixer.music.load(audio_clip_path)
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                pass  # Wait for audio to finish playing
 
-        except requests.RequestException as e:
-            print(f"OpenAI API call failed: {e}")
-            return None
+    # Handle exceptions for speech recognition
+    except sr.UnknownValueError:
+        print("Sorry, couldn't understand the audio.")
+    except sr.RequestError as e:
+        print(f"Could not request results from Google Speech Recognition service; {e}")
 
+def main():
+    while True:
+        listen_and_play_audio()
 
-class AudioPlayback:
-    def play_audio_clip(self, audio_path):
-        print(f"Playing audio clip: {audio_path}")
-        pygame.mixer.init()
-        pygame.mixer.music.load(audio_path)
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy():
-            time.sleep(1)
-        print("Audio clip playback finished.")
-
-
-class MainApp:
-    def __init__(self):
-        with open("commands.json", "r") as file:
-            self.commands_mapping = json.load(file)
-        self.audio_handler = AudioHandler()
-        self.nlp_service = NLPService()
-        self.audio_playback = AudioPlayback()
-
-    def run(self):
-        while True:
-            if keyboard.is_pressed("space"):
-                print("Spacebar pressed. Starting audio capture...")
-
-                audio_data = self.audio_handler.capture_audio()
-                if audio_data:
-                    transcribed_text = self.audio_handler.transcribe_audio(audio_data)
-                    if transcribed_text:
-                        nlp_response = self.nlp_service.process_text(transcribed_text, self.commands_mapping)
-                        if nlp_response:
-                            audio_clip_path = self.commands_mapping.get(nlp_response)
-                            if audio_clip_path:
-                                self.audio_playback.play_audio_clip(audio_clip_path)
-
-                time.sleep(1)  # Avoid rapid repetitions
-
-
-# Instantiate and run the app
-app = MainApp()
-app.run()
+if __name__ == "__main__":
+    main()
